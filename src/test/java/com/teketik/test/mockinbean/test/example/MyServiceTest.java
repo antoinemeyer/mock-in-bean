0b695d71876dd4d298aeb60301bec199f2cde964
antoine <antoine.meyer@trizic.com>
2022-01-09 11:02:44 -0500

    wip

--- src/test/java/com/teketik/test/mockinbean/test/example/MyServiceTest.java
+++ src/test/java/com/teketik/test/mockinbean/test/example/MyServiceTest.java
@@ -3,7 +3,6 @@ package com.teketik.test.mockinbean.test.example;
 import com.teketik.test.mockinbean.MockInBean;
 import com.teketik.test.mockinbean.SpyInBean;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +11,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class MyServiceTest {
 
-    //This needs to be an actual mock or spy to do the mockito verifications
-    //if this is a proxy, it does not work.
     @MockInBean(MyService.class)
     private ThirdPartyApiService thirdPartyApiService;
 
@@ -30,10 +27,5 @@ public class MyServiceTest {
         myService.doSomething();
         Mockito.verify(thirdPartyApiService).doSomethingOnThirdPartyApi(somethingExpensive);
     }
-    
-    @AfterEach
-    public void after() {
-        Mockito.validateMockitoUsage();
-    }
-    
+
 }

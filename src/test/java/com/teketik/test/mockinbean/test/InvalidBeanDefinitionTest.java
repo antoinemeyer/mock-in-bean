73c0098d2987261cbdb82792feefa81df8fcc37e
antoine <antoine.meyer@trizic.com>
2022-01-15 11:48:14 -0500

    test cleanup

--- src/test/java/com/teketik/test/mockinbean/test/InvalidBeanDefinitionTest.java
+++ src/test/java/com/teketik/test/mockinbean/test/InvalidBeanDefinitionTest.java
@@ -3,22 +3,40 @@ package com.teketik.test.mockinbean.test;
 import com.teketik.test.mockinbean.MockInBean;
 import com.teketik.test.mockinbean.MockInBeanTestExecutionListenerBridge;
 import com.teketik.test.mockinbean.MockInBeans;
+import com.teketik.test.mockinbean.test.InvalidBeanDefinitionTest.Config.TestComponentWithInvalidMockableComponent;
 import com.teketik.test.mockinbean.test.components.MockableComponent1;
 import com.teketik.test.mockinbean.test.components.TestComponent1;
-import com.teketik.test.mockinbean.test.components.TestComponentWithInvalidMockableComponent;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.test.context.TestContext;
 import org.springframework.test.context.TestExecutionListeners;
+
+import javax.annotation.PostConstruct;
 
 @TestExecutionListeners(
         value = { InvalidBeanDefinitionTest.TestExecutionListener.class },
         inheritListeners = false
 )
-@ActiveProfiles("altering-component")
 public class InvalidBeanDefinitionTest extends BaseTest {
+
+    @org.springframework.boot.test.context.TestConfiguration
+    static class Config {
+
+        @Component
+        class TestComponentWithInvalidMockableComponent {
+
+            @Autowired
+            private MockableComponent1 mockableComponent1;
+
+            @PostConstruct
+            public void alter() {
+                mockableComponent1 = new MockableComponent1();
+            }
+        }
+    }
 
     static class TestExecutionListener extends MockInBeanTestExecutionListenerBridge {
 
@@ -29,8 +47,8 @@ public class InvalidBeanDefinitionTest extends BaseTest {
                 Assertions.fail();
             } catch (IllegalArgumentException e) {
                 Assertions.assertEquals(
-                    "Resolved invalid target beans for definition Definition [name=mockableComponent1, resolvableType=com.teketik.test.mockinbean.test.components.MockableComponent1] in "
-                        + "InBeanDefinition [clazz=class com.teketik.test.mockinbean.test.components.TestComponentWithInvalidMockableComponent, name=null]",
+                    "Resolved invalid target beans for definition Definition [name=mockableComponent1, resolvableType=com.teketik.test.mockinbean.test.components.MockableComponent1] "
+                        + "in InBeanDefinition [clazz=class com.teketik.test.mockinbean.test.InvalidBeanDefinitionTest$Config$TestComponentWithInvalidMockableComponent, name=null]",
                     e.getMessage()
                 );
             }
